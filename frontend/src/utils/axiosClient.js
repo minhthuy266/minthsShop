2418a7ab14c16d324a13d481d7e72fde226ac385
Thuy Nguyen <thuy.nguyenminh266@gmail.com>
2025-04-19 12:56:38 +0700

    Update

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosClient = axios.create({
-  // baseURL: "https://minths-shop-api.onrender.com/",
+  baseURL: "https://minths-shop-api.onrender.com/",
 
-  baseURL: "http://localhost:5000",
+  // baseURL: "http://localhost:5000",
 });
